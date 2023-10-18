fbaeab840b4fcf036841a5be650c50a5e1a4c690
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2023-10-18 12:04:58 +0530

    Fixed some issues

--- TicketReservationApp/TicketReservation/ClientApp/src/components/BackOfficerDashboard.js
+++ TicketReservationApp/TicketReservation/ClientApp/src/components/BackOfficerDashboard.js
@@ -1,48 +1,13 @@
 import React from "react";
 
-const data = [
-  {
-    "title":"Backofficer Management",
-    "subtitle":"Manage backofficers here.",
-    "path":"/backofficers",
-    "buttonText":"Backofficer Management"
-  },
-  {
-    "title":"Update Status",
-    "subtitle":"Update status for traveller accounts.",
-    "path":"/update-status-management",
-    "buttonText":"Activate or Inactive the Traveller Accounts"
-  },
-  {
-    "title":"Train Management",
-    "subtitle":"Manage trains.",
-    "path":"/train-management",
-    "buttonText":"Train Management"
-  },
-  {
-    "title":"Schedule Management",
-    "subtitle":"Manage train schedules.",
-    "path":"/schedule-management",
-    "buttonText":"Schedule Management"
-  },
-  {
-    "title":"Agency Management",
-    "subtitle":"Manage agencies",
-    "path":"/travellers",
-    "buttonText":"Manage Agency"
-  },
-]
-
 const BackOfficerDashboard = () => {
   return (
     //Back Officer Dashboard
     <div className="container mt-5">
       <h1>Back Officer Dashboard</h1>
       <div className="row mt-4">
-        {
-          data.map((item ,i) => <Tile key={i} title={item.title} path={item.path} subtitle={item.subtitle} buttonText={item.buttonText} /> )
-        }
-        {/* <Tile title={"Back"} />
+        
+        <Tile title={"Back"} />
         <div className="col-md-4">
           <div className="card">
             <div className="card-body">
@@ -83,14 +48,14 @@ const BackOfficerDashboard = () => {
             <div className="card-body">
               <h5 className="card-title">Schedule Management</h5>
               <p className="card-text">
-                
+                To Schedule trains
               </p>
               <a href="/schedule-management" className="btn btn-primary">
                 Schedule Management
               </a>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
@@ -98,20 +63,4 @@ const BackOfficerDashboard = () => {
 
 export default BackOfficerDashboard;
 
-const Tile = ({title, subtitle , path  , buttonText}) => {
-  return(
-    <div className="col-md-4 mt-4">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">
-                {subtitle}
-              </p>
-              <a href={path} className="btn btn-primary">
-                {buttonText}
-              </a>
-            </div>
-          </div>
-        </div>
-  )
-}
+
